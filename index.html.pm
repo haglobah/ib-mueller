15b0dd1a4bbb96057ce85a09b221eee70d21581d
Beat Hagenlocher <bah@posteo.de>
2024-10-16 13:55:03 +0200

    Adjust once more

--- index.html.pm
+++ index.html.pm
@@ -2,11 +2,12 @@
 
 ◊div[#:class "mx-auto max-w-prose"]{
   ◊div[#:class "grid grid-cols-1 xs:grid-cols-2 justify-between gap-6 min-h-[90vh] xs:min-h-[60vh]"]{
-    ◊div[#:class "col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden bg-left-center xs:bg-[20%] bg-cover" #:style "background-image: url(mueller.jpg); background-size: 30rem"]{
-      ◊; ◊img[#:class "absolute" #:src "mueller.jpg"]
-      ◊h2[#:class "p-6 text-3xl font-light text-white"]{
-        Über mich
+    ◊div[#:class "col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden bg-[center_top_4rem] xs:bg-[20%]" #:style "background-image: url(mueller.jpg); background-size: 30rem; background-position: 0% 45%"]{
+      ◊div[#:class "flex flex-col justify-between bg-black/10 h-full w-full"]{
+        ◊h2[#:class "p-6 text-3xl text-white"]{
+          Über mich
         }
+      }
     }
     ◊div[#:class "col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden"]{
       ◊h2[#:class "p-6 text-3xl font-light"]{
