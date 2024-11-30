05f7aa6e14bf57e470faecd76d636ead226eb1a5
Beat Hagenlocher <bah@posteo.de>
2024-11-30 15:24:15 +0100

    Add card elements

--- index.html.pm
+++ index.html.pm
@@ -2,71 +2,29 @@
 
 ◊div[#:class "mx-auto max-w-prose"]{
   ◊div[#:class "grid grid-cols-1 xs:grid-cols-2 justify-between gap-6 min-h-[90vh] xs:min-h-[60vh]"]{
-    ◊div[#:class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden xs:bg-[20%]" #:style "background-image: url(mueller.jpg); background-size: 30rem; background-position: 0% 45%"]{
-      ◊div[#:class "flex flex-col justify-between bg-black/20 h-full w-full"]{
-        ◊h2[#:class "p-6 text-2xl xs:text-3xl text-white"]{
-          Über mich
-        }
-        ◊a[#:class "group/link cursor-pointer self-end" #:href "#about-me"]{
-          ◊span[#:class "absolute -inset-x-4 -inset-y-2.5 rounded"]{}
-          ◊div[#:class "bg-white i-heroicons:arrow-right -rotate-45 m-6 w-8 h-8 transition-all duration-200 group-hover:rotate-0"]{}
-        }
-      }
-    }
-    ◊div[#:class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden"]{
-      ◊div[#:class "flex flex-col h-full w-full"]{
-        ◊div[#:class "flex justify-between"]{
-          ◊h2[#:class "p-6 text-2xl xs:text-3xl font-light"]{
-            Konzeption
-            }
-          ◊a[#:class "group/link cursor-pointer self-end" #:href "#konzeption"]{
-              ◊span[#:class "absolute -inset-x-4 -inset-y-2.5 rounded"]{}
-              ◊div[#:class "bg-slate-800 i-heroicons:arrow-right -rotate-45 m-6 w-7 h-7 transition-all duration-200 group-hover:rotate-0"]{}
-          } 
-        }
-        ◊div[#:class "pl-6 pb-4 p-2 font-ibm-plex-sans font-bold"]{
-          ◊topics{
-            Neubau & Sanierung
-            Unabhängige Beratung
-            Strukturiertes Konzept
-            Projektentwicklung
-          }
+    ◊picture-card[#:title "Über mich" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 30rem; background-position: 0% 45%;" #:link "#about-me"]{}
+    ◊blank-card[#:title "Konzeption" #:link "#konzeption"]{
+      ◊div[#:class "pl-6 pb-4 p-2 font-ibm-plex-sans font-bold"]{
+        ◊topics{
+          Neubau & Sanierung
+          Unabhängige Beratung
+          Strukturiertes Konzept
+          Projektentwicklung
         }
       }
     }
-    ◊div[#:class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden"]{
-      ◊div[#:class "flex flex-col h-full w-full"]{
-        ◊div[#:class "flex justify-between"]{
-          ◊h2[#:class "p-6 text-2xl xs:text-3xl font-light"]{
-            Gutachten
-          }
-          ◊a[#:class "group/link cursor-pointer self-end" #:href "#gutachten"]{
-              ◊span[#:class "absolute -inset-x-4 -inset-y-2.5 rounded"]{}
-              ◊div[#:class "bg-slate-800 i-heroicons:arrow-right -rotate-45 m-6 w-7 h-7 transition-all duration-200 group-hover:rotate-0"]{}
-          }
-        }
-        ◊div[#:class "pl-6 pb-4 p-2 font-ibm-plex-sans font-bold"]{
-          ◊topics{
-            Technische Gebäudeausrüstung
-            Technical Due Diligence (TDD)
-            Sachkunde & Fachwissen
-            Gefährdungsanalyse TWVO
-            Hygieneinspektion VDI 6022/6023
-          }
+    ◊blank-card[#:title "Gutachten" #:link "#gutachten"]{
+      ◊div[#:class "pl-6 pb-4 p-2 font-ibm-plex-sans font-bold"]{
+        ◊topics{
+          Technische Gebäudeausrüstung
+          Technical Due Diligence (TDD)
+          Sachkunde & Fachwissen
+          Gefährdungsanalyse TWVO
+          Hygieneinspektion VDI 6022/6023
         }
       }
     }
-    ◊div[#:class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden xs:bg-[20%]" #:style "background-image: url(mueller.jpg); background-size: 30rem; background-position: 0% 45%"]{
-      ◊div[#:class "flex flex-col justify-between bg-black/20 h-full w-full"]{
-        ◊h2[#:class "p-6 text-2xl xs:text-3xl text-white"]{
-          Beratung
-        }
-        ◊a[#:class "group/link cursor-pointer self-end" #:href "#beratung"]{
-          ◊span[#:class "absolute -inset-x-4 -inset-y-2.5 rounded"]{}
-          ◊div[#:class "bg-white i-heroicons:arrow-right -rotate-45 m-6 w-8 h-8 transition-all duration-200 group-hover:rotate-0"]{}
-        }
-      }
-    }
+    ◊picture-card[#:title "Beratung" #:picture-style "background-image: url(assets/Beratung.svg); background-repeat: no-repeat; background-size: 18rem;" #:link "#beratung"]{}
   }
   ◊div[#:id "about-me" #:class "my-12 font-ibm-plex-sans scroll-mt-10"]{
     ◊h2[#:class "my-4 text-2xl xs:text-3xl font-ibm-plex-serif"]{
--- pollen.rkt
+++ pollen.rkt
@@ -12,3 +12,26 @@
     `(div ([class "flex flex-wrap gap-2"])
         ,@(map (curry splice-tag? 'span "after:content-['|'] last:after:content-[''] after:pl-2") top-list))))
 
+(define (picture-card #:title [title ""] #:picture-style [picture-style ""] #:link [link ""] . content)
+`(div
+  ([class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden xs:bg-[20%]"]
+  [style ,picture-style])
+  (div ([class "flex flex-col justify-between bg-black/40 h-full w-full"])
+       (h2 ([class "p-6 text-2xl xs:text-3xl font-medium text-white"])
+            ,title)
+       (a ([class "group/link cursor-pointer self-end"] [href ,link])
+          (span ([class "absolute -inset-x-4 -inset-y-2.5 rounded"]))
+          (div ([class "bg-white i-heroicons:arrow-right -rotate-45 m-6 w-8 h-8 transition-all duration-200 group-hover:rotate-0"])))
+    ,@content)))
+
+(define (blank-card #:title [title ""] #:link [link ""] . content)
+`(div
+  ([class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden"])
+  (div ([class "flex flex-col h-full w-full"])
+    (div ([class "flex justify-between"])
+       (h2 ([class "p-6 text-2xl xs:text-3xl"])
+            ,title)
+       (a ([class "group/link cursor-pointer self-end"] [href ,link])
+          (span ([class "absolute -inset-x-4 -inset-y-2.5 rounded"]))
+          (div ([class "bg-slate-800 i-heroicons:arrow-right -rotate-45 m-6 w-7 h-7 transition-all duration-200 group-hover:rotate-0"]))))
+    ,@content)))
