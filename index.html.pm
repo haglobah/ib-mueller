8b300a5f93b5895f13d9566c70c05c2911dd80f0
Beat Hagenlocher <bah@posteo.de>
2024-11-30 16:15:10 +0100

    Add sections with auto-links
    + various style things

--- index.html.pm
+++ index.html.pm
@@ -1,9 +1,9 @@
 #lang pollen
 
-◊div[#:class "mx-auto max-w-prose"]{
+◊div[#:class "mx-auto max-w-screen-md"]{
   ◊div[#:class "grid grid-cols-1 xs:grid-cols-2 justify-between gap-6 min-h-[90vh] xs:min-h-[60vh]"]{
-    ◊picture-card[#:title "Über mich" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 30rem; background-position: 0% 45%;" #:link "#about-me"]{}
-    ◊blank-card[#:title "Konzeption" #:link "#konzeption"]{
+    ◊picture-card[#:title "Über mich" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 30rem; background-position: 0% 45%;"]{}
+    ◊blank-card[#:title "Konzeption"]{
       ◊div[#:class "pl-6 pb-4 p-2 font-ibm-plex-sans font-bold"]{
         ◊topics{
           Neubau & Sanierung
@@ -13,7 +13,7 @@
         }
       }
     }
-    ◊blank-card[#:title "Gutachten" #:link "#gutachten"]{
+    ◊blank-card[#:title "Gutachten"]{
       ◊div[#:class "pl-6 pb-4 p-2 font-ibm-plex-sans font-bold"]{
         ◊topics{
           Technische Gebäudeausrüstung
@@ -24,62 +24,49 @@
         }
       }
     }
-    ◊picture-card[#:title "Beratung" #:picture-style "background-image: url(assets/Beratung.svg); background-repeat: no-repeat; background-size: 18rem;" #:link "#beratung"]{}
+    ◊picture-card[#:title "Beratung" #:picture-style "background-image: url(assets/Beratung.svg); background-repeat: no-repeat; background-size: 18rem;"]{}
   }
-  ◊div[#:id "about-me" #:class "my-12 font-ibm-plex-sans scroll-mt-10"]{
-    ◊h2[#:class "my-4 text-2xl xs:text-3xl font-ibm-plex-serif"]{
-      Über mich
-    }
-    ◊p[#:class "my-2"]{
+  ◊section-right[#:title "Über mich" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 34rem; background-position: 25% 50%;"]{
+    ◊par{
       In der heutigen komplexen Welt der technischen Gebäudeausrüstung sind fundierte Kenntnisse unerlässlich. Nach meiner Ausbildung zum Anlagenmechaniker in der Versorgungstechnik habe ich ein Diplomstudium an der Fachhochschule Esslingen abgeschlossen und mich als Fachplaner für vorbeugenden Brandschutz (EIPOS) weitergebildet. Mit über 20 Jahren Erfahrung als Projekt- und Bereichsleiter in einem mittelständischen Ingenieurbüro sowie im kommunalen Gebäudemanagement, bringe ich nicht nur Fachwissen, sondern auch die Fähigkeit mit, komplexe technische Herausforderungen zu meistern und effiziente Lösungen zu entwickeln.
     }
 
-    ◊p[#:class "my-2"]{
+    ◊par{
       Diese Expertise ist entscheidend, wenn es darum geht, Instandhaltungsbedarf frühzeitig zu erkennen und Risiken im Gebäudemanagement zu minimieren. In meiner Rolle als Dozent an der Hochschule Biberach habe ich zudem die Fähigkeit entwickelt, komplexe Themen verständlich zu vermitteln, was nicht nur das Lernen der Studierenden fördert, sondern auch die Qualität zukünftiger Fachkräfte sichert.
     }
 
-    ◊p[#:class "my-2"]{
+    ◊par{
       Mein Antrieb? Ich bin sportlich ambitioniert, naturverbunden, umweltbewusst und bereit, Neues mutig anzugehen. Dies spiegelt sich in meinem Engagement wider, innovative Lösungen zu finden, die nicht nur ressourcenschonend sind, sondern auch nachhaltige Ergebnisse liefern.
     }
 
-    ◊p[#:class "my-2"]{
+    ◊par{
       Ich arbeite stets auf Augenhöhe mit allen Beteiligten, wertschätzend und präzise. Durch diese Herangehensweise ermögliche ich eine offene Kommunikation, die es mir erlaubt, Probleme schnell zu identifizieren und effektive Lösungen zu entwickeln. Das Ergebnis? Ein reibungsloser Projektablauf, erhöhte Effizienz und nachhaltige Lösungen, die den Anforderungen der heutigen Zeit gerecht werden.
     }
   }
-  ◊div[#:id "konzeption" #:class "my-12 font-ibm-plex-sans scroll-mt-10"]{
-    ◊h2[#:class "my-4 text-2xl xs:text-3xl font-ibm-plex-serif"]{
-      Konzeption
-    }
-    ◊p[#:class "my-2"]{
+  ◊section-left[#:title "Konzeption" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 34rem; background-position: 25% 50%;"]{
+    ◊par{
       Die zunehmende Komplexität gebäudetechnischer Anlagen stellt Bauherren, Gebäudemanagement-Teams und Immobilienverwalter vor immer größere Herausforderungen. Ob bei der Planung eines Neubaus oder der Sanierung bestehender Gebäude – ein strukturiertes, verständliches Konzept ist entscheidend, um technische Möglichkeiten und gesetzliche Anforderungen in Einklang zu bringen.
     }
-    ◊p[#:class "my-2"]{
+    ◊par{
       Ich unterstütze Sie mit unabhängiger Beratung, um klare und umsetzbare Konzepte für Neubauprojekte und Bestandsgebäude zu entwickeln. Mein Fokus liegt dabei auf der Planung eines langfristigen Instandhaltungs- und Sanierungsmanagements für gebäudetechnische Anlagen – insbesondere in den Bereichen Heizung, Lüftung, Sanitär, Klimatechnik (HLSK) und Gebäudeautomation.
     }
   }
-  ◊div[#:id "gutachten" #:class "my-12 font-ibm-plex-sans scroll-mt-10"]{
-    ◊h2[#:class "my-4 text-2xl xs:text-3xl font-ibm-plex-serif"]{
-      Gutachten
-    }
-    ◊p[#:class "my-2"]{
+  ◊section-right[#:title "Gutachten" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 34rem; background-position: 25% 50%;"]{
+    ◊par{
       Die technische Gebäudeausrüstung wird immer komplexer. Bei Immobilienbewertungen stehen Eigentümer und Verwalter oft vor der Herausforderung, den Modernisierungs- und Instandsetzungsbedarf präzise zu bestimmen. Ohne fundierte Sachkunde und jahrelange Erfahrung sind solche Bewertungen schwer zu meistern – gerade, wenn es um die Einschätzung der Gebäudetechnik und den damit verbundenen Aufwand geht. Hier wird eine detaillierte Technical Due Diligence (TDD) unverzichtbar.
     }
-
-    ◊p[#:class "my-2"]{
+    ◊par{
       Besonders bei raumlufttechnischen Anlagen und Trinkwasserinstallationen sind spezifische Prüfungen gefordert. Diese müssen nicht nur den einschlägigen Verordnungen entsprechen, sondern auch den allgemein anerkannten Regeln der Technik folgen – eine Aufgabe, die oft überfordert und Zeit in Anspruch nimmt.
     }
-    ◊p[#:class "my-2"]{
+    ◊par{
       Ich biete Ihnen unabhängige Gutachten, detaillierte Gefährdungsanalysen nach der Trinkwasserverordnung (TWVO) und Hygieneinspektionen gemäß VDI 6022 und VDI 6023 an, um Ihre Gebäude auf Herz und Nieren zu prüfen und Ihnen klare Handlungsempfehlungen zu geben.
     }
   }
-  ◊div[#:id "beratung" #:class "my-12 font-ibm-plex-sans scroll-mt-10"]{
-    ◊h2[#:class "my-4 text-2xl xs:text-3xl font-ibm-plex-serif"]{
-      Beratung
-    }
-    ◊p[#:class "my-2"]{
+  ◊section-left[#:title "Beratung" #:picture-style "background-image: url(assets/mueller.jpg); background-repeat: no-repeat; background-size: 34rem; background-position: 25% 50%;"]{
+    ◊par{
       Ein effizienter, sicherer und der Nutzung angepasster Betrieb gebäudetechnischer Anlagen beginnt nicht erst mit der Inbetriebnahme oder wohl möglich mit der Übergabe an den Nutzer. Bereits bei der Planung von Gebäuden und der technischen Gebäudeausrüstung (TGA) sind eine Vielzahl von gewerkeübergreifenden Schnittstellen und Beziehungen zu beachten und zu koordinieren. Hier bestimmt sich größtenteils schon der Umfang der Instandhaltungsmaßnahmen während des gesamten Gebäudebetriebs.
     }
-    ◊p[#:class "my-2"]{
+    ◊par{
       Ich biete Ihnen hierzu fundierte Beratung mit jahrelangen Erkenntnissen aus der Planung, Realisierung und dem Betrieb gebäudetechnischer Anlagen an.
     }
   }
@@ -87,10 +74,10 @@
     ◊h2[#:class "my-4 text-2xl xs:text-3xl font-ibm-plex-serif"]{
       Kosten
     }
-    ◊p[#:class "my-2"]{
+    ◊par{
       Grundlage für die Ihnen angebotenen Leistungen ist ein Stundensatz von 120,- EUR/Stunde inkl. MwSt. Fahrtzeiten außerhalb von Tübingen werden zum vollen Stundensatz angerechnet zzgl. einem Kilometergeld von 0,50 EUR/km inkl. MwSt.
     }
-    ◊p[#:class "my-2"]{
+    ◊par{
       Gerne erstelle ich Ihnen nach Klärung Ihrer Leistungswünsche ein individuelles Angebot.
     }
   }
@@ -120,7 +107,7 @@
       }
       ◊div[#:class ""]{
         ◊a[#:href "mailto:info@ib-mueller-tuebingen.de" #:class ""]{
-          ◊p[#:class "my-2"]{
+          ◊par{
             ◊span[#:class "flex items-center justify-between gap-2 text-slate-700"]{
               ◊span[#:class "i-heroicons:envelope-16-solid bg-slate-700 w-6 h-6"]{}
               info@ib-mueller-tuebingen.de
@@ -128,7 +115,7 @@
           }
         }
         ◊a[#:href "tel:+491783235662" #:class ""]{
-          ◊p[#:class "my-2"]{
+          ◊par{
             ◊span[#:class "flex items-center justify-start gap-2 text-slate-700"]{
               ◊span[#:class "i-heroicons:phone-solid bg-slate-700 w-6 h-6"]{}
               +491783235662
--- pollen.rkt
+++ pollen.rkt
@@ -4,6 +4,12 @@
 
 (provide (all-defined-out))
 
+(define (title->link title)
+  (let* [(s (string-downcase title))
+         (s (string-replace s "ü" "ue" #:all? true))
+         (s (string-replace s " " "_" #:all? true))]
+    s))
+
 (define (splice-tag? tag class-name content)
 	(if (equal? content "") "" `(,tag [[class ,class-name]] ,content)))
 
@@ -12,26 +18,48 @@
     `(div ([class "flex flex-wrap gap-2"])
         ,@(map (curry splice-tag? 'span "after:content-['|'] last:after:content-[''] after:pl-2") top-list))))
 
-(define (picture-card #:title [title ""] #:picture-style [picture-style ""] #:link [link ""] . content)
+(define (picture-card #:title [title ""] #:picture-style [picture-style ""] . content)
 `(div
-  ([class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden xs:bg-[20%]"]
+  ([class "group col-span-1 shadow hover:shadow-lg rounded-2xl relative overflow-hidden xs:bg-[20%]"]
   [style ,picture-style])
   (div ([class "flex flex-col justify-between bg-black/40 h-full w-full"])
        (h2 ([class "p-6 text-2xl xs:text-3xl font-medium text-white"])
             ,title)
-       (a ([class "group/link cursor-pointer self-end"] [href ,link])
+       (a ([class "group/link cursor-pointer self-end"] [href ,(string-append "#" (title->link title))])
           (span ([class "absolute -inset-x-4 -inset-y-2.5 rounded"]))
           (div ([class "bg-white i-heroicons:arrow-right -rotate-45 m-6 w-8 h-8 transition-all duration-200 group-hover:rotate-0"])))
     ,@content)))
 
-(define (blank-card #:title [title ""] #:link [link ""] . content)
+(define (blank-card #:title [title ""] . content)
 `(div
-  ([class "group col-span-1 shadow hover:shadow-lg rounded-3xl relative overflow-hidden"])
+  ([class "group col-span-1 shadow hover:shadow-lg rounded-2xl relative overflow-hidden"])
   (div ([class "flex flex-col h-full w-full"])
     (div ([class "flex justify-between"])
        (h2 ([class "p-6 text-2xl xs:text-3xl"])
             ,title)
-       (a ([class "group/link cursor-pointer self-end"] [href ,link])
+       (a ([class "group/link cursor-pointer self-end"] [href ,(string-append "#" (title->link title))])
           (span ([class "absolute -inset-x-4 -inset-y-2.5 rounded"]))
           (div ([class "bg-slate-800 i-heroicons:arrow-right -rotate-45 m-6 w-7 h-7 transition-all duration-200 group-hover:rotate-0"]))))
     ,@content)))
+
+(define (section-right #:title [title ""] #:picture-style [picture-style ""] . content)
+  `(div ([id ,(title->link title)] [class "my-12 font-ibm-plex-sans scroll-mt-10"])
+    (h2 ([class "my-8 text-2xl xs:text-3xl font-ibm-plex-serif"])
+      ,title)
+    (div ([class "md:grid md:grid-cols-9 md:gap-4"])
+      (div ([class "h-96 mr-4 my-4 md:col-span-3 rounded-2xl object-fit"] [style ,picture-style]))
+      (div ([class "md:col-span-6"])
+        ,@content))))
+
+(define (section-left #:title [title ""] #:picture-style [picture-style ""] . content)
+  `(div ([id ,(title->link title)] [class "my-12 font-ibm-plex-sans scroll-mt-10"])
+    (h2 ([class "my-8 text-2xl xs:text-3xl font-ibm-plex-serif"])
+      ,title)
+    (div ([class "md:grid md:grid-cols-9 md:gap-4"])
+      (div ([class "md:col-span-6"])
+        ,@content)
+      (div ([class "h-96 mr-4 my-4 md:col-span-3 rounded-2xl object-fit"] [style ,picture-style])))))
+
+(define (par . content)
+  `(p ([class "my-2"])
+      ,@content))
